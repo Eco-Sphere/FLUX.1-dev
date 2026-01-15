ace23e714f592137070e4ded01099780abe33e7f
tallmessiwu <wujunlin20@huawei.com>
2026-01-15 17:24:06 +0800

    增加支持mxfp8量化类型

--- inference_flux.py
+++ inference_flux.py
@@ -50,8 +50,8 @@ def parse_arguments():
     parser.add_argument("--max_num_prompt", type=int, default=0, help="limit the prompt number[0 indicates no limit]")
     parser.add_argument("--info_file_save_path", type=str, default="./image_info.json", help="path to save image info")
     parser.add_argument("--width", type=int, default=1024, help='Image size width')
-    parser.add_argument("--height", type=int, default=1024, help='Image size height')  
-    parser.add_argument("--infer_steps", type=int, default=50, help="Inference steps") 
+    parser.add_argument("--height", type=int, default=1024, help='Image size height')
+    parser.add_argument("--infer_steps", type=int, default=50, help="Inference steps")
     parser.add_argument("--seed", type=int, default=42, help="A seed for all the prompts")
     parser.add_argument("--use_cache", action="store_true", help="turn on dit cache or not")
     parser.add_argument("--batch_size", type=int, default=1, help="prompt batch size")
@@ -71,7 +71,7 @@ def parse_arguments():
     )
     # ======================== Quant config ========================
     parser.add_argument("--use_quant", action="store_true", help="turn on quant or not")
-    parser.add_argument("--quant_type", choices=["w8a16", "w8a8_dynamic"], default="w8a8_dynamic", help="specify quant type")
+    parser.add_argument("--quant_type", choices=["w8a16", "w8a8_dynamic", "w8a8_mxfp8"], default="w8a8_dynamic", help="specify quant type")
     # ======================== Test config ========================
     parser.add_argument("--prompt", type=str, default="Beautiful illustration of The ocean. in a serene landscape, magic realism, narrative realism, beautiful matte painting, heavenly lighting, retrowave, 4 k hd wallpaper", help="default prompt")
     return parser.parse_args()
@@ -103,7 +103,7 @@ def transfer_nd_to_nz(pipe):
 def init_cv_parallel(cp_level):
     """
     Initialize computer vision parallel processing based on the specified level.
-    
+
     Args:
         cp_level (int): The level of parallel processing to enable.
                         - 0: No parallel processing
@@ -152,10 +152,10 @@ def initialize_pipeline(args):
         FluxPipeline.extract_init_dict = classmethod(replace_tp_extract_init_dict)
 
     pipe = FluxPipeline.from_pretrained(args.path, torch_dtype=torch.bfloat16, local_files_only=True)
-    
+
     if args.sequence_parallel:
         pipe.transformer.pos_embed.enable_seq_parallel()
-        
+
     if args.use_cache:
         d_stream_config = CacheConfig(
             method="dit_block_cache",
@@ -256,7 +256,7 @@ def initialize_pipeline(args):
 
                 for name, module in pipe.transformer.named_children():
                     if name not in ["transformer_blocks", "single_transformer_blocks"]:
-                        module.to(device) 
+                        module.to(device)
 
     if bool(os.environ.get("USE_NZ", 0)):
         transfer_nd_to_nz(pipe)
@@ -278,11 +278,11 @@ def infer(args):
         os.makedirs(args.save_path, mode=0o640)
     check_dir_safety(args.save_path)
     check_param_valid(args.height, args.width, args.infer_steps)
-    
+
     total_nums = 6
     warmup_nums = 3
     time_consume = 0.0
-    
+
     for i in range(total_nums):
         prompts = [args.prompt]
         torch.npu.synchronize()
@@ -295,7 +295,7 @@ def infer(args):
             num_inference_steps=args.infer_steps,
             max_sequence_length=512,
             use_cache=args.use_cache,
-        )        
+        )
         torch.npu.synchronize()
         end_time = time.time() - start_time
         print(f"The inference time of the {i} image is: {end_time}")
@@ -310,7 +310,7 @@ def infer(args):
         else:
             image_save_path = os.path.join(args.save_path, f"{i}.png")
             image[0][0].save(image_save_path)
-        
+
     print(f"Average inference time is: {time_consume / (total_nums - warmup_nums)}")
 
     return
